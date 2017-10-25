c29f7cebc829d45b358ba17b4b612ea5aae85b67
Alex Kursell <alexkursell@gmail.com>
2017-10-25 12:48:50 -0400

    Cleaned up servo test.

--- tests/servotest/servotest.ino
+++ tests/servotest/servotest.ino
@@ -2,23 +2,30 @@
 
 #define LOCK_POSITION 2000
 #define OPEN_POSITION 1200
+#define SERVO_PIN 3
+#define MIC_PIN A0
 
 
-  Servo servo;
+const Servo servo;
+
+void lock_box(){
+  servo.writeMicroseconds(LOCK_POSITION);
+}
+
+void open_box(){
+  servo.writeMicroseconds(OPEN_POSITION);
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(11, OUTPUT);
-
-  servo.attach(11);
-  
-
+  //Initialize servo.
+  pinMode(SERVO_PIN, OUTPUT);
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
- // put your main code here, to run repeatedly:
-  servo.writeMicroseconds(OPEN_POSITION);
+  //5 second cycle of locked and unlocked.
+  open_box();
   delay(5000);
-  servo.writeMicroseconds(LOCK_POSITION);
+  lock_box();
   delay(5000);
-
 }
